f36a19b2ba1522f4df529bdd887d4cdf00f27339
christian6404 <christian6404@gmail.com>
2023-05-07 19:14:52 -0400

    Finished test and shapes constructor

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest')
-const shapes = require('./lib/shapes')
+const {Triangle, Square, Circle} = require('./lib/shapes')
 
-const questionsAndAnswers = [{
+class Blueprint{
+   constructor(){
+      this.shapeE = ''
+      this.textE = ''
+   }
+   render() {
+      return `<svg width="300" height="200">${this.shapeE}${this.textE}</svg>`
+   }
+
+   setShapeE(shape) {
+      this.shapeE = shape.render()
+   }
+   setTextE(text, color) {
+      this.textElement = `<text x="120" y="100" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+   }
+}
+
+
+
+
+
+const questionsAndAnswers = [
+ {
     type: 'input',
     message: 'What text would you like to include in your logo? (MAX OF 3 CHARACTERS) :',
     name: 'text',
@@ -23,5 +45,5 @@ const questionsAndAnswers = [{
     type: 'input',
     message: 'What color would you like the shape to be? (Normal colors or hexadeciaml number)',
     name: 'shapeColor'
- },
+ }
 ];
