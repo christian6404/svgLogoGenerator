0c8292852d8e5db96151317dc4a2befdc86ee6e0
christian6404 <christian6404@gmail.com>
2023-05-08 15:08:22 -0400

    fixed bug, examples, added readme
    Fixed bugs in index.js that would return undefined as the shape color. Added the examples folder with some examples. Added full readme.md

---
+++ README.md
@@ -0,0 +1,39 @@
+# SVG_LOGO_GENERATOR ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Description
+
+Generates a logo using user input in a cli
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](https://opensource.org/licenses/MIT) file for details.
+
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+Install node.js, open the "index.js" file wth a terminal, run npm i to install inquirer, then simply run node index.html and answer the questions prompted, a new logo.svg will be generated with user inputs.
+
+## Usage
+
+To generate a logo for any purposes needed.
+
+## Contributing
+
+Christian, Smith, christian6404
+
+## Tests
+
+Tested for each shape and gave sample inputs, one test per shape available so 3 in total.
+
+## Questions
+
+N/A
--- index.js
+++ index.js
@@ -9,14 +9,14 @@ class Blueprint{
       this.textE = ''
    }
    render() {
-      return `<svg width="300" height="200">${this.shapeE}${this.textE}</svg>`
+      return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeE}${this.textE}</svg>`
    }
 
    setShapeE(shape) {
       this.shapeE = shape.render()
    }
    setTextE(text, color) {
-      this.textElement = `<text x="120" y="100" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+      this.textE = `<text x="150" y="120" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
    }
 }
 
@@ -24,7 +24,7 @@ class Blueprint{
 
 
 
-const questionsAndAnswers = [
+const questionsAnduserInput = [
  {
     type: 'input',
     message: 'What text would you like to include in your logo? (MAX OF 3 CHARACTERS) :',
@@ -47,3 +47,64 @@ const questionsAndAnswers = [
     name: 'shapeColor'
  }
 ];
+
+
+
+
+function writeToSVG(fileName, data) {
+	console.log('Starting write to file')
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("SVG File Generated!");
+    });
+}
+
+async function initilization() {
+   var svgString = "";
+   var svgFile = "logo.svg";
+ 
+   const userInput = await inquirer.prompt(questionsAnduserInput);
+   console.log(userInput)
+   var userText = "";
+   if (userInput.text.length > 0 && userInput.text.length < 4) {
+      userText = userInput.text;
+   } else {
+      console.log("Please only enter 1-3 characters");
+      return;
+   }
+  
+   let fontColor = userInput.textColor;
+   let shapeColor = userInput.shapeColor;
+   let shapeType = userInput.shape;
+ 
+   let shapeInstance;
+   if (shapeType === "Square" || shapeType === "square") {
+       shapeInstance = new Square();
+      //  shapeInstance.setColor(shapeColor);
+   }
+   else if (shapeType === "Circle" || shapeType === "circle") {
+      shapeInstance = new Circle();
+      // shapeInstance.setColor(shapeColor);
+   }
+   else if (shapeType === "Triangle" || shapeType === "triangle") {
+      shapeInstance = new Triangle();
+      // shapeInstance.setColor(shapeColor);
+   }
+   else {
+      console.log("Invalid shape!");
+      return;
+   }
+ 
+   shapeInstance.setColor(shapeColor);
+ 
+   let svg = new Blueprint();
+   svg.setTextE(userText, fontColor);
+   svg.setShapeE(shapeInstance);
+   svgString = svg.render();
+   
+   console.log("Shape generation complete!");
+   writeToSVG(svgFile, svgString); 
+ }
+initilization()
